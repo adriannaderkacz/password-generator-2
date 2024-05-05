3fce7740e526d278591f65974a5c988aa64c45e5
Adrianna Derkacz <a.derkacz@gmail.com>
2024-05-05 23:47:16 +0200

    Added new elements to html, updated styling and updated js

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,16 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordOne = document.getElementById("password-one-el")
-let passwordTwo = document.getElementById("password-two-el")
+let passwordEl = document.getElementById("password-el")
 let buttonEl = document.getElementById("button-el")
 
 buttonEl.addEventListener("click", function() {
-    let passwordOneText = []
-    let passwordTwoText = []
+    let passwordText = []
 
     for (let i = 0; i < 15; i++) {
-        let randomIndexOne = Math.floor(Math.random() * characters.length)
-        let randomIndexTwo = Math.floor(Math.random() * characters.length)
-
-        passwordOneText += characters[randomIndexOne]
-        passwordTwoText += characters[randomIndexTwo]
+        let randomIndex = Math.floor(Math.random() * characters.length)
+        passwordText += characters[randomIndex]
     }
 
-    passwordOne.textContent = passwordOneText
-    passwordTwo.textContent = passwordTwoText
+    passwordEl.textContent = passwordText
 })
